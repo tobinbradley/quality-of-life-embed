0c4fb17452ccf50d25056068b760747321642998
tobinbradley <tobin.bradley@gmail.com>
2017-01-30 14:32:36 -0500

    add back click event

--- app/js/modules/breaks.js
+++ app/js/modules/breaks.js
@@ -1,3 +1,4 @@
 let breaksGnBu5 = ["rgb(238,250,227)", "rgb(186,228,188)", "rgb(123,204,196)", "rgb(67,162,202)", "rgb(8,104,172)"];
+let breaksYlGnBl = ["#ffffcc", "#a1dab4", "#41b6c4", "#2c7fb8", "#253494"];
 
-export default { breaksGnBu5 };
+export default { breaksGnBu5, breaksYlGnBl };
