03b3d10bdec332b4ee8066006480d097e30c6fe2
tobinbradley <tobin.bradley@gmail.com>
2017-08-04 14:15:14 -0400

    add continuous scale breaks

--- app/js/modules/breaks.js
+++ app/js/modules/breaks.js
@@ -1,4 +1,5 @@
 let breaksGnBu5 = ["rgb(238,250,227)", "rgb(186,228,188)", "rgb(123,204,196)", "rgb(67,162,202)", "rgb(8,104,172)"];
 let breaksYlGnBl = ["#ffffcc", "#a1dab4", "#41b6c4", "#2c7fb8", "#253494"];
+let breaksRange = ["#FFFFE0", "#B0062C"];
 
-export default { breaksGnBu5, breaksYlGnBl };
+export { breaksGnBu5, breaksYlGnBl, breaksRange };
