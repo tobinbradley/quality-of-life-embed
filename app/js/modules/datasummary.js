3bf2885f3ca3a4e6c868a6daa041d18152a9fd33
tobinbradley <tobin.bradley@gmail.com>
2017-08-21 10:28:02 -0400

    genericize data summary

--- app/js/modules/datasummary.js
+++ app/js/modules/datasummary.js
@@ -5,55 +5,31 @@ import selectGroups from '../../../data/config/selectgroups';
 
 function dataSummary(appState) {
     let summary = {
-        years: appState.metric.years, 
+        years: appState.metric.years,
         year: appState.year,
-        selected: appState.selected,       
+        selected: appState.selected,
         values: {
-            'Jurisdiction': {},
-            'City Council': {},
-            'County Commission': {},
             'Neighborhood': {}
         }
     };
 
     // Geography Groups
-    let selectGroupKeys;
+    let selectGroupCategory = Object.keys(selectGroups);
 
-    selectGroupKeys = Object.keys(selectGroups.Jurisdiction);
-    for (let g = 0; g < selectGroupKeys.length; g++) {
-        let groupArray = [];
-        let keys = selectGroups.Jurisdiction[selectGroupKeys[g]];
-        for (let i = 0; i < appState.metric.years.length; i++) {
-            let groupVal = calcValue(appState.metric.data, appState.metric.config.type, appState.metric.years[i], keys);
-            groupVal = prettyNumber(groupVal, appState.metric.config.decimals, appState.metric.config.prefix, appState.metric.config.suffix);
-            groupArray.push(groupVal);
+    selectGroupCategory.forEach(category => {
+        summary.values[category] = {};
+        let selectGroupKeys = Object.keys(selectGroups[category]);
+        for (let g = 0; g < selectGroupKeys.length; g++) {
+            let groupArray = [];
+            let keys = selectGroups[category][selectGroupKeys[g]];
+            for (let i = 0; i < appState.metric.years.length; i++) {
+                let groupVal = calcValue(appState.metric.data, appState.metric.config.type, appState.metric.years[i], keys);
+                groupVal = prettyNumber(groupVal, appState.metric.config.decimals, appState.metric.config.prefix, appState.metric.config.suffix);
+                groupArray.push(groupVal);
+            }
+            summary.values[category][selectGroupKeys[g]] = groupArray;
         }
-        summary.values["Jurisdiction"][selectGroupKeys[g]] = groupArray;
-    }
-
-    selectGroupKeys = Object.keys(selectGroups["City Council"]);
-    for (let g = 0; g < selectGroupKeys.length; g++) {
-        let groupArray = [];
-        let keys = selectGroups["City Council"][selectGroupKeys[g]];
-        for (let i = 0; i < appState.metric.years.length; i++) {
-            let groupVal = calcValue(appState.metric.data, appState.metric.config.type, appState.metric.years[i], keys);
-            groupVal = prettyNumber(groupVal, appState.metric.config.decimals, appState.metric.config.prefix, appState.metric.config.suffix);
-            groupArray.push(groupVal);
-        }
-        summary.values["City Council"]['District ' + selectGroupKeys[g]] = groupArray;
-    }
-
-    selectGroupKeys = Object.keys(selectGroups["County Commission"]);
-    for (let g = 0; g < selectGroupKeys.length; g++) {
-        let groupArray = [];
-        let keys = selectGroups["County Commission"][selectGroupKeys[g]];
-        for (let i = 0; i < appState.metric.years.length; i++) {
-            let groupVal = calcValue(appState.metric.data, appState.metric.config.type, appState.metric.years[i], keys);
-            groupVal = prettyNumber(groupVal, appState.metric.config.decimals, appState.metric.config.prefix, appState.metric.config.suffix);
-            groupArray.push(groupVal);
-        }
-        summary.values["County Commission"]['District ' + selectGroupKeys[g]] = groupArray;
-    }
+    });
 
     // County
     let areaArray = [];
@@ -81,7 +57,7 @@ function dataSummary(appState) {
             selectedArray.push(selectedValue);
         }
         summary.values.Neighborhood = selectedArray;
-    }    
+    }
 
     return summary;
 }
