2d671b26ad193b72bf5aba242c5005887104d46b
tobinbradley <tobin.bradley@gmail.com>
2017-12-29 15:11:01 -0500

    make prettier

--- app/js/modules/datasummary.js
+++ app/js/modules/datasummary.js
@@ -2,16 +2,16 @@ import {calcValue} from './metric_calculations';
 import {prettyNumber} from './number_format';
 import selectGroups from '../../../data/config/selectgroups';
 
-
 function dataSummary(appState) {
     let summary = {
         years: appState.metric.years,
         year: appState.year,
         selected: appState.selected,
+        data: appState.metric.data,
         values: {
-            'Neighborhood': {},
-            'Jurisdiction': {}
-        }
+            Neighborhood: {},
+            Jurisdiction: {},
+        },
     };
 
     // Geography Groups
@@ -24,8 +24,18 @@ function dataSummary(appState) {
             let groupArray = [];
             let keys = selectGroups[category][selectGroupKeys[g]];
             for (let i = 0; i < appState.metric.years.length; i++) {
-                let groupVal = calcValue(appState.metric.data, appState.metric.config.type, appState.metric.years[i], keys);
-                groupVal = prettyNumber(groupVal, appState.metric.config.decimals, appState.metric.config.prefix, appState.metric.config.suffix);
+                let groupVal = calcValue(
+                    appState.metric.data,
+                    appState.metric.config.type,
+                    appState.metric.years[i],
+                    keys,
+                );
+                groupVal = prettyNumber(
+                    groupVal,
+                    appState.metric.config.decimals,
+                    appState.metric.config.prefix,
+                    appState.metric.config.suffix,
+                );
                 groupArray.push(groupVal);
             }
             summary.values[category][selectGroupKeys[g]] = groupArray;
@@ -37,24 +47,54 @@ function dataSummary(appState) {
     let keys = Object.keys(appState.metric.data.map);
     for (let i = 0; i < appState.metric.years.length; i++) {
         let areaValue;
-        if (appState.metric.config.world_val && appState.metric.config.world_val[`y_${appState.metric.years[i]}`]) {
-            areaValue = appState.metric.config.world_val[`y_${appState.metric.years[i]}`];
-            areaValue = prettyNumber(areaValue, appState.metric.config.decimals, appState.metric.config.prefix, appState.metric.config.suffix);
+        if (
+            appState.metric.config.world_val &&
+            appState.metric.config.world_val[`y_${appState.metric.years[i]}`]
+        ) {
+            areaValue =
+                appState.metric.config.world_val[
+                    `y_${appState.metric.years[i]}`
+                ];
+            areaValue = prettyNumber(
+                areaValue,
+                appState.metric.config.decimals,
+                appState.metric.config.prefix,
+                appState.metric.config.suffix,
+            );
         } else {
-            areaValue = calcValue(appState.metric.data, appState.metric.config.type, appState.metric.years[i], keys);
-            areaValue = prettyNumber(areaValue, appState.metric.config.decimals, appState.metric.config.prefix, appState.metric.config.suffix);
+            areaValue = calcValue(
+                appState.metric.data,
+                appState.metric.config.type,
+                appState.metric.years[i],
+                keys,
+            );
+            areaValue = prettyNumber(
+                areaValue,
+                appState.metric.config.decimals,
+                appState.metric.config.prefix,
+                appState.metric.config.suffix,
+            );
         }
         areaArray.push(areaValue);
     }
     summary.values.Jurisdiction.County = areaArray;
 
-
     // selected values
     if (appState.selected.length > 0) {
         let selectedArray = [];
         for (let i = 0; i < appState.metric.years.length; i++) {
-            let selectedValue = calcValue(appState.metric.data, appState.metric.config.type, appState.metric.years[i], appState.selected);
-            selectedValue = prettyNumber(selectedValue, appState.metric.config.decimals, appState.metric.config.prefix, appState.metric.config.suffix);
+            let selectedValue = calcValue(
+                appState.metric.data,
+                appState.metric.config.type,
+                appState.metric.years[i],
+                appState.selected,
+            );
+            selectedValue = prettyNumber(
+                selectedValue,
+                appState.metric.config.decimals,
+                appState.metric.config.prefix,
+                appState.metric.config.suffix,
+            );
             selectedArray.push(selectedValue);
         }
         summary.values.Neighborhood = selectedArray;
